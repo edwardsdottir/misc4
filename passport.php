9adb38da19633c6b8883aa88b0d490847c924f4d
edwardsdottir <68348069+edwardsdottir@users.noreply.github.com>
2021-01-22 15:04:58 -0500

    Update passport.php

--- passport.php
+++ passport.php
@@ -2,7 +2,7 @@
 <pre>
 <?php
   if (!(preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}$/', $_GET['ip']))) {
-     header("Location: example3.php?ip=127.0.0.1");
+     header("Location: network.php?ip=127.0.0.1");
   }
   system("ping -c 2 ".$_GET['ip']);
 ?>
